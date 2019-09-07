f78a14336c609fd0541f71c22e4f1783ff78e410
Sam <sam@picoded.com>
2019-09-08 01:30:37 +0800

    Still trying to get the scripts to work

--- EctoInstallMigrate.sh
+++ EctoInstallMigrate.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-export SECRET_KEY_BASE=$(mix phx.gen.secret)
 MIX_ENV=prod mix ecto.create
 MIX_ENV=prod mix ecto.migrate
--- InstallDependenciesCompile.sh
+++ InstallDependenciesCompile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mix local.hex --force
 mix local.rebar --force
-mix deps.get
+mix deps.get --only-prod
 MIX_ENV=prod mix compile
