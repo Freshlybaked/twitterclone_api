7c6758ce3cb847c5d4571bbec62b704d4c7a63ff
Sam <sam@picoded.com>
2019-09-08 08:44:21 +0800

    might need sudo to run these 2 commands

--- InstallDependenciesCompile.sh
+++ InstallDependenciesCompile.sh
@@ -2,5 +2,5 @@
 cd /home/ubuntu/twitterclone_api
 mix local.hex --force
 mix local.rebar --force
-mix deps.get --only-prod
-MIX_ENV=prod mix compile
+sudo mix deps.get --only-prod
+sudo MIX_ENV=prod mix compile
