198c6912047e6f1779887568984f0d27a4a986bf
Sam <sam@picoded.com>
2019-09-08 10:51:56 +0800

    Made ec2 tag untag script filter more precise

--- EC2TagUntag.sh
+++ EC2TagUntag.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-RESOURCE_ID=$(aws ec2 describe-instances --filters Name=tag:green,Values=true --query Reservations[*].Instances[*].InstanceId | jq -r '.[] | .[0]')
+RESOURCE_ID=$(aws ec2 describe-instances --filters Name=tag:green,Values=true Name=tag:twitterclone_api,Values=true --query Reservations[*].Instances[*].InstanceId | jq -r '.[] | .[0]')
 aws ec2 create-tags --resources $RESOURCE_ID --tags Key=blue,Value=true
 aws ec2 delete-tags --resources $RESOURCE_ID --tags Key=blue,Value=true
